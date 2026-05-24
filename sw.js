769a08bddec1a6c79e1c37a369ce8d92754dddaf
Sinaya Naik <sinayanaik@gmail.com>
2026-05-24 15:16:45 +0530

    Fix list width overflow causing microscopic text scale

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "markdown-flashcards-v20260524-8";
+const CACHE_NAME = "markdown-flashcards-v20260524-9";
 const APP_SHELL = [
   "./",
-  "./styles.css?v=20260524-8",
-  "./app.js?v=20260524-8",
+  "./styles.css?v=20260524-9",
+  "./app.js?v=20260524-9",
   "./manifest.webmanifest",
   "./fevicon.png",
   "./icons/icon-192.png",
